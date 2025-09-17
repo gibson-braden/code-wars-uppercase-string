3798041ea6cbf177ea5691605185c478b3d2e428
gibson-braden <bradengibson1213@gmail.com>
2025-09-17 14:54:46 -0400

    Delete uppercase_string.py

--- uppercase_string.py
+++
@@ -1,8 +0,0 @@
-def main():
-    testString = input("Input a sentence to test if it is all uppercase")
-    if testString.isupper():
-        print("True")
-    else:
-        print("False")
-
-main()
