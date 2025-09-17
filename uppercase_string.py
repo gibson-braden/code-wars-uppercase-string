d95ee3882376d560d0733f55a4ded373cceeda74
gibson-braden <bradengibson1213@gmail.com>
2025-09-17 14:53:54 -0400

    Add files via upload

---
+++ uppercase_string.py
@@ -0,0 +1,8 @@
+def main():
+    testString = input("Input a sentence to test if it is all uppercase")
+    if testString.isupper():
+        print("True")
+    else:
+        print("False")
+
+main()
